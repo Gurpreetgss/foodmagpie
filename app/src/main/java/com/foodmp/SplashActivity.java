ad8a905c068fddc6ad24cf5e258c543077edc842
ashish sharma <ashish.as.sharma999@gmail.com>
2021-10-03 14:05:21 -0400

    Splah_screen

--- app/src/main/java/com/foodmp/SplashActivity.java
+++ app/src/main/java/com/foodmp/SplashActivity.java
@@ -2,14 +2,43 @@ package com.foodmp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN = 5000;
+    Animation topAnim, bottomAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+
+        ImageView imglogo=findViewById(R.id.applogo);
+        TextView slogan = findViewById(R.id.anim_3);
+
+        imglogo.setAnimation(topAnim);
+    
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashActivity.this, Welcome.class));
+
+            }
+        }, SPLASH_SCREEN);
 
     }
 }
